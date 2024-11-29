806233444729c39e6641d805836def4a43e946f9
YUMA829 <148312448+YUMA829@users.noreply.github.com>
2024-11-29 14:51:07 +0900

    Add files via upload

---
+++ summary/home/script.js
@@ -0,0 +1,9 @@
+const urlParams = new URLSearchParams(window.location.search);
+const points = urlParams.get('points');
+const rank = urlParams.get('rank');
+
+// ポイントと順位を表示
+document.getElementById('points').textContent = `現在のポイント: ${points || '不明'}`;
+document.getElementById('rank').textContent = `現在の順位: ${rank || '不明'}`;
+
+
